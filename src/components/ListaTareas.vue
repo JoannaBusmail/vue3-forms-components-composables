2540e7a7430cc20ec9f807e65d117a59d7cdd013
JoannaBusmail <joanna.busmail@gmail.com>
2023-11-21 16:17:03 +0100

    chore: add lista tareas y store

---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,55 @@
+<template>
+    <h5>Lista de Tareas</h5>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Categorias</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Número</th>
+                <th scope="col">Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr
+                v-for="(tarea, index) in tareasList"
+                :key="tarea.id"
+            >
+                <th scope="row">{{ tarea.id }}</th>
+                <td>{{ tarea.nombre }}</td>
+                <td>{{ tarea.categorias.join(', ') }}</td>
+                <td>{{ tarea.estado }}</td>
+                <td>{{ tarea.numero }}</td>
+                <td> <button
+                        class="btn btn-danger btn-sm"
+                        @click="removeTarea(tarea.id)"
+                    >Eliminar</button>
+                </td>
+                <td> <router-link
+                        class="btn btn-warning btn-sm"
+                        :to="`/editar/${tarea.id}`"
+                    >Editar</router-link>
+                </td>
+            </tr>
+        </tbody>
+
+
+    </table>
+</template>
+
+
+<script setup>
+
+import { useTareaStore } from '@/stores/tareas'
+import { storeToRefs } from 'pinia'
+
+
+const useTarea = useTareaStore()
+const { removeTarea } = useTarea
+const { tareasList } = storeToRefs(useTarea)
+
+
+
+
+</script>
--- src/stores/tareas.js
+++ src/stores/tareas.js
@@ -1,10 +1,11 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import router from '../router'
 
 export const useTareaStore = defineStore('tareas', () => {
 
   //refs
-  const tareas = ref([])
+  const tareasList = ref([])
   const tarea = ref({
     id: '',
     nombre: '',
@@ -13,13 +14,35 @@ export const useTareaStore = defineStore('tareas', () => {
     numero: 0
   })
 
+  const tareasListUpdated = ref(false);
+ if(localStorage.getItem('tareas')){
+  tareasList.value = JSON.parse(localStorage.getItem('tareas'))
+ }
 //actions
-const setTareas = (tarea) =>{
-  tareas.value.push(tarea)
+const addTareas = (tarea) =>{
+  tareasList.value.push(tarea)
+  localStorage.setItem('tareas', JSON.stringify(tareasList.value))
 
-  console.log(tarea)
+  console.log(tareasList.value)
+
 }
 
+const removeTarea = (id) =>{
+  tareasList.value = tareasList.value.filter(tarea => tarea.id !== id)
+  localStorage.setItem('tareas', JSON.stringify(tareasList.value))
+}
 
-  return { tareas, tarea, setTareas }
+const getTarea = (id) =>{
+  tarea.value = tareasList.value.find(tarea => tarea.id === id)
+  console.log(tarea.value)
+}
+
+const updateTarea = (tarea) =>{
+  tareasList.value = tareasList.value.map(t => t.id === tarea.id ? tarea : t)
+  localStorage.setItem('tareas', JSON.stringify(tareasList.value))
+  tareasListUpdated.value = !tareasListUpdated.value;
+  router.push('/')
+}
+
+  return { tareasList, tarea, addTareas, removeTarea, getTarea, updateTarea, tareasListUpdated}
 })
